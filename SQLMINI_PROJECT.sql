4c71302d69e2a50cbb32757628fa47eb0abcf39f
Sidhant khosla <sid@Sidhants-MacBook-Pro.local>
2020-11-27 14:39:53 +0000

    Code update and word file

--- SQLMINI_PROJECT.sql
+++ SQLMINI_PROJECT.sql
@@ -1,13 +1,19 @@
-/* que 1*/
-SELECT CustomerID, CompanyName, Address FROM Customers
+-- 1.1	Write a query that lists all Customers in either Paris or London. Include Customer ID, Company Name and all address fields.
+
+SELECT CustomerID, CompanyName, Address
+FROM Customers
 WHERE City='Paris' OR City='London'
 
----Q1.2
+
+-- 1.2 List all products stored in bottles. 
+
 SELECT *
 FROM Products p
 WHERE p.QuantityPerUnit LIKE '%bottle%'
 
----Q1.2
+
+-- 1.3 Repeat question above, but add in the Supplier Name and Country. 
+
 SELECT p.ProductID AS "Product ID",
     p.ProductName AS "Product Name",
     s.CompanyName AS "Company Name",
@@ -16,7 +22,9 @@ FROM Products p
 JOIN Suppliers s ON p.SupplierID = s.SupplierID 
 WHERE p.QuantityPerUnit LIKE '%bottle%'
 
-/* Que 1.4 */
+
+-- 1.4 Write an SQL Statement that shows how many products there are in each category. Include Category Name in result set and list the highest number first. 
+
 SELECT c.CategoryName AS "Category",
     COUNT(*) AS "Number of products in Category"
 FROM Products p
@@ -24,26 +32,31 @@ JOIN Categories c ON p.CategoryID = c.CategoryID
 GROUP BY c.CategoryName
 ORDER BY COUNT(*) DESC
 
-/* Que 1.5 */
+
+-- 1.5 List all UK employees using concatenation to join their title of courtesy, first name and last name together. Also include their city of residence. 
+
 
 SELECT CONCAT(e.TitleOfCourtesy,' ',e.FirstName,' ',e.LastName) AS "Employee",
     e.City AS "City of Residence"
 FROM Employees e
 WHERE e.Country = 'UK'
 
+-- 1.6 List Sales Totals for all Sales Regions (via the Territories table using 4 joins) with a Sales Total greater than 1,000,000. Use rounding or FORMAT to present the numbers. 
 
 
-/* Que 1.6 Removing multiple teritories for employees */
-SELECT r.RegionDescription, ROUND(SUM(od.Quantity * od.UnitPrice * (1 - od.Discount)),2) total_sales 
+SELECT r.RegionDescription, 
+ROUND(SUM(od.Quantity * od.UnitPrice * (1 - od.Discount)),2) total_sales 
 FROM  [Order Details] od
 INNER JOIN Orders o ON o.OrderID = od.OrderID
 INNER JOIN EmployeeTerritories et ON et.EmployeeID = o.EmployeeID
 INNER JOIN Territories t ON t.TerritoryID = et.TerritoryID
 INNER JOIN Region r ON r.RegionID = t.RegionID
-GROUP  BY r.RegionDescription
+GROUP BY r.RegionDescription
 HAVING ROUND(SUM(od.Quantity * od.UnitPrice),2) >1000000
 
---- Que 1.7
+
+-- 1.7 Count how many Orders have a Freight amount greater than 100.00 and either USA or UK as Ship Country. 
+
 SELECT COUNT(OrderID) AS "Orders", o.ShipCountry AS "Ship Country"
 FROM Orders o
 WHERE ShipCountry IN ('UK', 'USA')
@@ -51,17 +64,18 @@ AND o.Freight > 100
 GROUP BY o.ShipCountry
 
 
---- Que 1.8
-USE Northwind
-
+-- 1.8 Write an SQL Statement to identify the Order Number of the Order with the highest amount(value) of discount applied to that order. 
 
 SELECT (od.UnitPrice*od.Quantity*od.Discount) AS "Biggest_amount_of_discount", od.OrderID
 FROM [Order Details] od 
 WHERE od.UnitPrice*od.Discount*od.Quantity = (SELECT MAX(orr.UnitPrice*orr.Discount*orr.Quantity) FROM [Order Details] orr)
 ORDER BY Biggest_amount_of_discount DESC
 
+-- 2.1 Write the correct SQL statement to create the following table: 
+-- Spartans Table – _include details about all the Spartans on this course. Separate Title,
+-- First Name and Last Name into separate columns,
+-- and include University attended, course taken and mark achieved. Add any other columns you feel would be appropriate. 
 
----EX2
 CREATE DATABASE sidhant_db
 USE sidhant_db
 CREATE TABLE Spartans_table
@@ -76,6 +90,9 @@ CREATE TABLE Spartans_table
 
 )
 
+-- IMPORTANT NOTE: For data protection reasons do NOT include date of birth in this exercise. 
+-- 2.2 Write SQL statements to add the details of the Spartans in your course to the table you have created.
+
 SELECT * FROM Spartans_table --SELECT everything from a table
 INSERT INTO
 Spartans_table(Title, firstName, lastName, universityAttended, courseTaken, markAchieved, tieGame)
@@ -84,31 +101,26 @@ VALUES
 ('Mr.', 'Nathan', 'Johnston', 'Susex', 'IDK', '100', 'STRONG'),
 ('Mr.', 'Asakar', 'Hussain', 'Middlesex', 'CS','100', 'BOW-TIE'),
 ('Mr.' , 'Sidhant', 'Khosla', 'Brunel', 'Business','100' , 'FANCY')
----
-USE Northwind
 
----Q 3.1
-SELECT CONCAT(e.TitleOfCourtesy, e.FirstName, ' ' ,e.LastName) AS "Employee Name",CONCAT(id.TitleOfCourtesy, id.FirstName, ' ' ,id.LastName) AS 'Report To'
+-- 3.1 List all Employees from the Employees table and who they report to. 
+
+SELECT CONCAT(e.TitleOfCourtesy, e.FirstName, ' ' ,e.LastName) AS "Employee Name",
+CONCAT(id.TitleOfCourtesy, id.FirstName, ' ' ,id.LastName) AS 'Report To'
 FROM Employees e
 LEFT JOIN Employees id
 ON id.EmployeeID=e.ReportsTo
 
-----
-USE Northwind
--- Q3.2
-SELECT ROUND(SUM((1-od.Discount)*od.Quantity * od.UnitPrice),2) AS "sales", s.CompanyName
-FROM Orders o
-INNER JOIN [Order Details] od 
-ON od.OrderID = o.OrderID
-INNER JOIN Products p 
-ON p.ProductID = od.ProductID
-INNER JOIN Suppliers s 
-ON s.SupplierID = p.SupplierID
-GROUP BY s.CompanyName
-HAVING ROUND(SUM((1-od.Discount)*od.Quantity * od.UnitPrice),2) > 10000.00
-ORDER BY sales DESC
+-- 3.2 List all Suppliers with total sales over $10,000 in the Order Details table. Include the Company Name from the Suppliers Table 
 
--- Q3.3
+SELECT s.CompanyName,
+    ROUND(SUM(od.Quantity*(od.UnitPrice-od.UnitPrice*od.Discount)),2) AS "Total Sales"
+FROM Suppliers s
+JOIN Products p ON s.SupplierID = p.SupplierID
+JOIN [Order Details] od ON p.ProductID = od.ProductID
+GROUP BY s.CompanyName
+HAVING SUM(od.Quantity*(od.UnitPrice-od.UnitPrice*od.Discount)) > 10000
+
+-- 3.3 List the Top 10 Customers YTD for the latest year in the Orders file. Based on total value of orders shipped. No Excel required
 
 SELECT TOP 10 c.CompanyName AS "Company Name",
     ROUND(SUM(od.Quantity*(od.UnitPrice-od.UnitPrice*od.Discount)),2) AS "Total Value Of Orders Shipped"
@@ -119,28 +131,10 @@ WHERE YEAR(o.OrderDate)=(SELECT MAX(YEAR(oo.OrderDate)) FROM Orders oo)
 GROUP BY c.CompanyName
 ORDER BY "Total value of orders shipped" DESC
 
+-- 3.4 Plot the Average Ship Time by month for all data in the Orders Table using a line chart as below. 
 
---- Q3.4
-USE Northwind
-
-
-
-SELECT AVG(CAST(DATEDIFF(d, o.OrderDate, o.ShippedDate) AS Decimal(4,2))) AS "Average Ship time", FORMAT(o.OrderDate, 'MMM,yyyy') AS Dates
+SELECT AVG(DATEDIFF(DAY,o.OrderDate,o.ShippedDate)) AS "Average Ship Time",
+    FORMAT(o.OrderDate,'MMM-yyyy') AS "Month"
 FROM Orders o
-GROUP BY FORMAT(o.OrderDate, 'MMM,yyyy')
-ORDER BY YEAR(FORMAT(o.OrderDate, 'MMM,yyyy')), MONTH(FORMAT(o.OrderDate, 'MMM,yyyy'))
-
-SELECT *
-FROM Orders
-
-
-
-
-
-
-
-
-
-
-
-/* https://social.msdn.microsoft.com/Forums/en-US/c29c94ad-f4ed-4ad2-b376-05b88cf70a80/list-sales-totals-for-all-sales-regions-via-the-territories-table-using-4-joins-with-a-sales-total?forum=sqlgetstarted*/
+GROUP BY FORMAT(o.OrderDate,'MMM-yyyy'),DATEPART(YEAR, o.OrderDate), DATEPART(MONTH, o.OrderDate)
+ORDER BY DATEPART(YEAR, o.OrderDate),DATEPART(MONTH, o.OrderDate)
